a5569ab993d24ecda338651eb52e2d996e8212f3
Jon <jong649@gmail.com>
2019-10-27 15:40:20 -0700

    Updates

---
+++ precache-manifest.060ce63690c731c30990e9db83b7dfb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57401eec96ec235130c2855ebf0625f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f0419575d474459a857b",
+    "url": "/monsters-rolodex/static/css/main.60e95d7a.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "f0419575d474459a857b",
+    "url": "/monsters-rolodex/static/js/main.a146cb5b.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
